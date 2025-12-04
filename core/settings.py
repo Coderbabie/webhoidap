efeb54446443a9df6f072378f028c6fe2c4f4afa
code20428 <email_cua_ban@domain.com>
2025-12-04 17:16:15 +0700

    update

--- core/settings.py
+++ core/settings.py
@@ -1,115 +1,38 @@
-import configparser
+import os
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-CONFIG = configparser.ConfigParser(interpolation=None)
-CONFIG.read('config.ini')
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+SECRET_KEY = os.environ.get("SECRET_KEY", "unsafe-secret-key")
 
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-SECRET_KEY = CONFIG['Django']['SECRET_KEY']
+ALLOWED_HOSTS = ["*"]
 
-DEBUG = True
+AUTH_USER_MODEL = "auth.User"
 
-AUTH_USER_MODEL = CONFIG['Django']['AUTH_USER_MODEL']
-
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
-
-LANGUAGE_CODE = CONFIG['Django']['LANGUAGE_CODE']
-
-TIME_ZONE = CONFIG['Django']['TIME_ZONE']
+LANGUAGE_CODE = "en-us"
+TIME_ZONE = "Asia/Ho_Chi_Minh"
 
 USE_I18N = True
-
 USE_TZ = True
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
-MEDIA_URL = '/images/'
+MEDIA_URL = "/images/"
+MEDIA_ROOT = BASE_DIR / "static/images"
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-
-MEDIA_ROOT = BASE_DIR / 'static/images'
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-ROOT_URLCONF = 'core.urls'
-
-WSGI_APPLICATION = 'core.wsgi.application'
+ROOT_URLCONF = "core.urls"
+WSGI_APPLICATION = "core.wsgi.application"
 
 CORS_ALLOW_ALL_ORIGINS = True
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
-
-INSTALLED_APPS = [
-    'jazzmin',
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-
-    'base.apps.BaseConfig',
-
-    'rest_framework',
-    "corsheaders",
-]
-
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    "corsheaders.middleware.CorsMiddleware",
-
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
-
-
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
-
-
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
-
-
